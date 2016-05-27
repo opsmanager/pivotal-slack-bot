f8fe4e4e5eb57a4c4ced5dfb7782dda12ef0fb1f
Brennan Neoh, Jude Cooray, Rui Baltazar and Sam Kah Chiin <pair+brennan+jude+rui+sam@opsmanager.com>
2016-05-27 17:21:49 +0800

    Removed authenticated listener

--- main.js
+++ main.js
@@ -8,10 +8,6 @@ rtm.start();
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-});
-
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
   if(/\#\d+/.test(message.text)) {
     id = message.text.match(/\#\d+/)[0];
